97c5676215628f83c783eb01c9eaf6c5ad7cd8f2
anabia <absdo@iscte-iul.pt>
2025-09-18 17:51:09 +0100

    Foi comentada.

--- src/main/java/com/vaadin/starter/bakery/ui/views/storefront/events/EditEvent.java
+++ src/main/java/com/vaadin/starter/bakery/ui/views/storefront/events/EditEvent.java
@@ -3,9 +3,21 @@ package com.vaadin.starter.bakery.ui.views.storefront.events;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.ComponentEvent;
 
+/**
+ * Event triggered when an edit action is performed on a UI {@link Component}.
+ * <p>
+ * This event can be used to notify that the user has initiated an edit
+ * operation, for example on an order item or another editable element
+ * in the storefront.
+ */
 public class EditEvent extends ComponentEvent<Component> {
 
-	public EditEvent(Component source) {
-		super(source, false);
-	}
+    /**
+     * Creates a new {@code EditEvent}.
+     *
+     * @param source the {@link Component} where the edit action occurred
+     */
+    public EditEvent(Component source) {
+        super(source, false);
+    }
 }
--- src/main/java/com/vaadin/starter/bakery/ui/views/storefront/events/ProductChangeEvent.java
+++ src/main/java/com/vaadin/starter/bakery/ui/views/storefront/events/ProductChangeEvent.java
@@ -4,17 +4,34 @@ import com.vaadin.flow.component.ComponentEvent;
 import com.vaadin.starter.bakery.backend.data.entity.Product;
 import com.vaadin.starter.bakery.ui.views.orderedit.OrderItemEditor;
 
+/**
+ * Event that represents a change in a product within the {@link OrderItemEditor} component.
+ * <p>
+ * This event is fired when a product is modified or selected in the editor.
+ * </p>
+ */
 public class ProductChangeEvent extends ComponentEvent<OrderItemEditor> {
 
-	private final Product product;
+    private final Product product;
 
-	public ProductChangeEvent(OrderItemEditor component, Product product) {
-		super(component, false);
-		this.product = product;
-	}
+    /**
+     * Creates a new {@code ProductChangeEvent}.
+     *
+     * @param component the {@link OrderItemEditor} instance that fired the event
+     * @param product the {@link Product} that was changed
+     */
+    public ProductChangeEvent(OrderItemEditor component, Product product) {
+        super(component, false);
+        this.product = product;
+    }
 
-	public Product getProduct() {
-		return product;
-	}
+    /**
+     * Returns the {@link Product} associated with this event.
+     *
+     * @return the changed product
+     */
+    public Product getProduct() {
+        return product;
+    }
 
 }
